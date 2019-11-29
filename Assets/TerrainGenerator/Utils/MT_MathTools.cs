ca2ee408eb70eeb1eb0f84955cb986f1611c279f
André Delran <Delran@hotmail.fr>
2019-11-29 10:05:18 +0100

    Real modulus operation

---
+++ Assets/TerrainGenerator/Utils/MT_MathTools.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MT_MathTools
+{
+    public static int Mod(int _nb, int _mod)
+    {
+        return (_nb % _mod + _mod) % _mod;
+    }
+}
