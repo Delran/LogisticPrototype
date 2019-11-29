ecf71396e508eb1d735fd90532f946e7cca0f331
André Delran <Delran@hotmail.fr>
2019-11-29 16:57:21 +0100

    Terrain mesh generation from two dimentional array

--- Assets/TerrainGenerator/Utils/Test/TG_Test.cs
+++ Assets/TerrainGenerator/Utils/Test/TG_Test.cs
@@ -17,18 +17,78 @@ public class TG_Test : MonoBehaviour
 
     void TestMesh()
     {
-        GameObject _plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        MeshFilter _planeMesh = _plane.GetComponent<MeshFilter>();
-        MeshRenderer _planeRenderer = _plane.GetComponent<MeshRenderer>();
+        //GameObject _plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        //MeshFilter _planeMesh = _plane.GetComponent<MeshFilter>();
+        //MeshRenderer _planeRenderer = _plane.GetComponent<MeshRenderer>();
 
-        Vector3[] _vercicles = _planeMesh.mesh.vertices;
+        int _side = 9;
+        int _mapSize = _side * _side;
+        Vector3[] _vercicles = new Vector3[_mapSize];
+
+        Vector3 _initialPos = Vector3.zero;
+
+        //To get the number of triangles, we count the number of squares wich is number of vertice on one side - 1 power 2
+        // 9 vertices each side = (9 - 1)^2 squares
+        int _trianglesSize = (_side - 1) * (_side - 1);
+        //There is 2 triangle by square and each triangle has two points so triangles = squares * 2 * 3;
+        _trianglesSize *= 6;
+
+        int[] _triangles = new int[_trianglesSize];
+
+        Debug.Log(_trianglesSize);
+        Debug.Log(_mapSize);
+
+        float _step = 1;
+        int _triangleIt = 0;
+        for (int i = 0, _row = 0; i < _mapSize; i++)
+        {
+            int y = i % _side;
+            if (i != 0 && y == 0) _row++;
+            _vercicles[i] = new Vector3(_row * _step, 0 , y * _step) + _initialPos;
+            if (_row != 0)
+            {
+                if (y != _side)
+                {
+                    _triangles[_triangleIt++] = i - _side;
+                    _triangles[_triangleIt++] = i;
+                    _triangles[_triangleIt++] = i - (_side - 1);
+                }
+                if (y != 0)
+                {
+                    _triangles[_triangleIt++] = i-1;
+                    _triangles[_triangleIt++] = i;
+                    _triangles[_triangleIt++] = i-_side;
+                }
+            }
+        }
+
+        /*
+                         if (y != _side)
+                {
+                    _triangles[_triangleIt] = i - _side;
+                    _triangleIt++;
+                    _triangles[_triangleIt] = i;
+                    _triangleIt++;
+                    _triangles[_triangleIt] = i - (_side - 1);
+                    _triangleIt++;
+                }
+                if (y != 0)
+                {
+                    _triangles[_triangleIt] = i-1;
+                    _triangleIt++;
+                    _triangles[_triangleIt] = i;
+                    _triangleIt++;
+                    _triangles[_triangleIt] = i-_side;
+                    _triangleIt++;
+                }
+         */
 
         Mesh mesh = new Mesh();
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
         mesh.vertices = _vercicles;
         //mesh.uv = _planeMesh.mesh.uv;
-        mesh.triangles = _planeMesh.mesh.triangles;
-        Debug.Log(mesh.triangles.Length);
+        mesh.triangles = _triangles;
+        //mesh.normals = _planeMesh.mesh.normals;
         gameObject.AddComponent<MeshRenderer>();
     }
 
